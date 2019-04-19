89e729f747baeb2e01f8150ae53591eaf75e46b4
nathan cerisara <cerisara.nathan@gmail.com>
2019-04-19 07:38:25 +0100

    a

--- supersnake.py
+++ supersnake.py
@@ -31,12 +31,24 @@ def bouton(x,y,tx,ty,cl):
 def texte(texte,x,y,taille,cl):
     fenetre.blit( pygame.font.SysFont(ffont,taille).render(texte,taille,cl) , [rx(x),ry(y)] )
 
-clobjs=[(70,70,70),(150,150,150),(230,230,230),(70,0,0),(150,0,0),(230,0,0)]
+clobjs=[(70,70,70),(150,150,150),(230,230,230),(0,70,0),(0,150,0),(0,230,0),(70,0,0),(150,0,0),(230,0,0),(70,70,0),(150,150,0),(230,230,0)]
 clm=(245,245,245)
 
 pkeys=[[K_UP,K_DOWN,K_LEFT,K_RIGHT],[K_KP8,K_KP2,K_KP4,K_KP6],[K_o,K_l,K_k,K_m],[K_t,K_g,K_f,K_h]]
+pkaff=["up-down-left-right","num8-num2-num4-num6","o-l-k-m","t-g-f-h"]
+
+ldifs=["easy","medium","hard","hardcore"]
+
+gamemodes=["normal","battle-royale","time match","max size match","max points match","teams match"]
 
 ################################jeu
+
+class Team:
+    def __init__(self,nom,cl):
+        self.nom=nom
+        self.cl=cl
+        self.snakes=[]
+        self.points=0        
 
 class Snake:
     def __init__(self,x,y,cl,tmin):
@@ -62,6 +74,7 @@ class Snake:
         self.perdu=False
         self.player=0
         self.agr=tmin-1
+        self.team=None
     def bouger(self,aa):
         if aa == "up": self.sens=aa
         elif aa == "down": self.sens=aa
@@ -148,14 +161,14 @@ def bot(s,snakes,cubes,objs):
     
 
 
-def ccc(snakes,cubes,mis,tx,ty,objs,dtc,tac,nbobjs,nbv):
+def ccc(snakes,cubes,mis,tx,ty,objs,dtc,tac,nbobjs,nbv,mode,tmin):
   if time.time()-dtc >= tac:
     ccccc=[]
     dtc=time.time()
     while len(objs)<nbobjs:
         px,py=random.randint(5,tx-5),random.randint(5,ty-5)
         while [px,py] in cubes: px,py=random.randint(5,tx-5),random.randint(5,ty-5)
-        objs.append([px,py,random.randint(0,5)])
+        objs.append([px,py,random.randint(0,11)])
     for s in snakes:
         if not s.perdu:
             if s.player==0: bot(s,snakes,cubes,objs)
@@ -197,22 +210,60 @@ def ccc(snakes,cubes,mis,tx,ty,objs,dtc,tac,nbobjs,nbv):
                         elif s.cubes[0][3]=="down": s.cubes[0][1]-=1
                         elif s.cubes[0][3]=="left": s.cubes[0][0]+=1
                         elif s.cubes[0][3]=="right": s.cubes[0][0]-=1
-                        for c in s.cubes:
-                            l=c[2]
-                            cl=[l[0]+20,l[1]+20,l[2]+20]
-                            if cl[0] > 255: cl[0]=255
-                            if cl[1] > 255: cl[1]=255
-                            if cl[2] > 255: cl[2]=255
-                            cl=(cl[0],cl[1],cl[2])
-                            cubes.append( [c[0],c[1],cl] )
+                        if mode == 0 or mode==1:
+                            for c in s.cubes: cubes.append( [c[0],c[1]] )
+                        else:
+                            s.points-=10*len(s.cubes)
+                            cl=s.cube[0][2]
+                            s.cubes=[]
+                            px,py=random.choice(5,tx-5),random.randint(5,ty-5)
+                            while [px,py] in cubes: px,py=random.choice(5,tx-5),random.randint(5,ty-5)
+                            s.cubes.append( [px,py,cl,random.choice(["up","down","left","right"]),random.choice(["up","down","left","right"]) ] )
+                            s.agr=tmin
                     for o in objs:
                         if c[0]==o[0] and c[1]==o[1]:
                             if o[2]==0: s.agr+=1
                             elif o[2]==1: s.agr+=3
                             elif o[2]==2: s.agr+=5
-                            elif o[2]==3: s.points+=10*len(s.cubes)
-                            elif o[2]==4: s.points+=30*len(s.cubes)
-                            elif o[2]==5: s.points+=50*len(s.cubes)
+                            elif o[2]==3:
+                                if mode == 5: s.team.points+=10*len(s.cubes)
+                                else: s.points+=10*len(s.cubes)
+                            elif o[2]==4:
+                                if mode == 5: s.team.points+=30*len(s.cubes)
+                                else: s.points+=30*len(s.cubes)
+                            elif o[2]==5:
+                                if mode == 5: s.team.points+=50*len(s.cubes)
+                                else: s.points+=50*len(s.cubes)
+                            elif o[2]==6:
+                                for hh in range(1):
+                                    if len(s.cubes)>3:
+                                        del(s.cubes[len(s.cubes)-1])
+                            elif o[2]==7:
+                                for hh in range(3):
+                                    if len(s.cubes)>3:
+                                        del(s.cubes[len(s.cubes)-1])
+                            elif o[2]==8:
+                                for hh in range(5):
+                                    if len(s.cubes)>3:
+                                        del(s.cubes[len(s.cubes)-1])
+                            elif o[2]==9:
+                                for hh in range(1):
+                                    if len(s.cubes)>3:
+                                        c=s.cubes[len(s.cubes)-1]
+                                        del(s.cubes[len(s.cubes)-1])
+                                        cubes.append([c[0],c[1]])
+                            elif o[2]==10:
+                                for hh in range(3):
+                                    if len(s.cubes)>3:
+                                        c=s.cubes[len(s.cubes)-1]
+                                        del(s.cubes[len(s.cubes)-1])
+                                        cubes.append([c[0],c[1]])
+                            elif o[2]==11:
+                                for hh in range(5):
+                                    if len(s.cubes)>3:
+                                        c=s.cubes[len(s.cubes)-1]
+                                        del(s.cubes[s.cubes.index(c)])
+                                        cubes.append([c[0],c[1]])
                             try: del(objs[objs.index(o)])
                             except: print("error")
             if s.agr>0:
@@ -221,7 +272,8 @@ def ccc(snakes,cubes,mis,tx,ty,objs,dtc,tac,nbobjs,nbv):
     
   return snakes,cubes,mis,tx,ty,objs,dtc,tac,nbv
 
-def game(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin):
+def game(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin,keysp,tps,palierpoint,paliertaille,nbteams,teamsp):
+    teams=[]
     bpx,bpy=rx(50),ry(50)
     tc=rx(750)/tx
     dtc=time.time()
@@ -242,7 +294,7 @@ def game(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin):
         while [px,py] in cubes: px,py=random.randint(5,tx-5),random.randint(5,ty-5)
         snakes.append( Snake(px,py,rcl(),tmin) )
         if s<nbj:
-            snakes[s].keys=pkeys[s]
+            snakes[s].keys=pkeys[keysp[s]]
             snakes[s].player=s+1
     nbv=len(snakes)
     affgame(snakes,cubes,mis,tx,ty,objs,affbords,affquadr)
@@ -255,9 +307,9 @@ def game(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin):
     pygame.display.update()
     time.sleep(3)
     encourg=True
-    while nbv > 0:
+    while encourg:
         affgame(snakes,cubes,mis,tx,ty,objs,affbords,affquadr)
-        snakes,cubes,mis,tx,ty,objs,dtc,tac,nbv=ccc(snakes,cubes,mis,tx,ty,objs,dtc,tac,nbobjs,nbv)
+        snakes,cubes,mis,tx,ty,objs,dtc,tac,nbv=ccc(snakes,cubes,mis,tx,ty,objs,dtc,tac,nbobjs,nbv,mode,tmin)
         for event in pygame.event.get():
             if event.type==QUIT: exit()
             elif event.type==KEYDOWN:
@@ -267,6 +319,22 @@ def game(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin):
                     elif event.key==s.keys[1] and s.cubes[0][3]!="up": s.bouger("down")
                     elif event.key==s.keys[2] and s.cubes[0][3]!="right": s.bouger("left")
                     elif event.key==s.keys[3] and s.cubes[0][3]!="left": s.bouger("right")
+        if mode==0: #mode normal
+            if nbv<=0: encourg=False
+        elif mode==1: #battle-royale
+            if nbv<=1: encourg=False
+        elif mode==2: #time-game
+            if tps<=0: encourg=False
+        elif mode==3: #the first at big size
+            for s in snakes:
+                if len(scubes)>=paliertaille: encourg=True
+        elif mode==4: #the first at a lot of points
+            for s in snakes:
+                if s.points>=palierpoints: encourg=True
+        elif mode==5: #teams match
+            lpp=0
+            for t in range(len(teams)):
+                if teams[t].points>teams[lpp].points: lpp=t
     lp1=0
     lp2=0
     lp3=0
@@ -322,12 +390,11 @@ def game(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin):
 
 ##################################menu
 
-ldifs=["easy","medium","hard","hardcore"]
-
-def affmenu(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin):
+def affmenu(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin,keysp,tps,palierpoints,paliertaille,nbteams,teamsp):
     bts=[]
-    for w in range(20): bts.append(None)
+    for w in range(40): bts.append(None)
     fenetre.fill(clf)
+    texte("SUPERSNAKE",50,50,50,rcl())
     bts[0]=bouton(300,600,200,100,(150,150,50))
     texte("play",320,620,20,(0,0,0))
     #nbj
@@ -363,15 +430,97 @@ def affmenu(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin):
     #affbords
     clb=(250,0,0)
     if affbords: clb=(0,250,0)
-    bts[11]=bouton(400,50,25,20,clb)
-    texte("show edges",430,50,20,(0,0,0))
+    bts[11]=bouton(100,400,25,20,clb)
+    texte("show edges",130,400,20,(0,0,0))
     #affquadr
     clb=(250,0,0)
     if affquadr: clb=(0,250,0)
-    bts[12]=bouton(400,90,25,20,clb)
-    texte("show grid",430,90,20,(0,0,0))
+    bts[12]=bouton(100,450,25,20,clb)
+    texte("show grid",130,450,20,(0,0,0))
+    #keys_players
+    texte("keys",500,100,20,(0,0,0))
+    #p1
+    if nbj>=1:
+        bts[13]=bouton(430,140,25,25,(200,200,200))
+        bts[14]=bouton(670,140,25,25,(200,200,200))
+        texte("<",435,140,25,(0,0,0))
+        texte(">",675,140,25,(0,0,0))
+        texte(pkaff[keysp[0]],470,140,20,(0,0,0))
+        texte("player1 : ",300,140,20,(0,0,0))
+    #p2
+    if nbj>=2:
+        bts[15]=bouton(430,180,25,25,(200,200,200))
+        bts[16]=bouton(670,180,25,25,(200,200,200))
+        texte("<",435,180,25,(0,0,0))
+        texte(">",675,180,25,(0,0,0))
+        texte(pkaff[keysp[1]],470,180,20,(0,0,0))
+        texte("player2 : ",300,180,20,(0,0,0))
+    #p3
+    if nbj>=3:
+        bts[17]=bouton(430,220,25,25,(200,200,200))
+        bts[18]=bouton(670,220,25,25,(200,200,200))
+        texte("<",435,220,25,(0,0,0))
+        texte(">",675,220,25,(0,0,0))
+        texte(pkaff[keysp[2]],470,220,20,(0,0,0))
+        texte("player3 : ",300,220,20,(0,0,0))
+    #p4
+    if nbj>=4:
+        bts[19]=bouton(430,260,25,25,(200,200,200))
+        bts[20]=bouton(670,260,25,25,(200,200,200))
+        texte("<",435,260,25,(0,0,0))
+        texte(">",675,260,25,(0,0,0))
+        texte(pkaff[keysp[3]],470,260,20,(0,0,0))
+        texte("player4 : ",300,260,20,(0,0,0))
+    #teams_players
+    if mode==5:
+        #p1
+        if nbj>=1:
+            bts[21]=bouton(700,140,50,25,(200,200,200))
+            texte("<>",705,140,25,(0,0,0))
+            texte("team "+str(teamsp[0]+1),760,140,20,(0,0,0))
+        #p2
+        if nbj>=2:
+            bts[22]=bouton(700,180,50,25,(200,200,200))
+            texte("<>",705,180,25,(0,0,0))
+            texte("team "+str(teamsp[0]+1),760,180,20,(0,0,0))
+        #p3
+        if nbj>=3:
+            bts[23]=bouton(700,220,50,25,(200,200,200))
+            texte("<>",705,220,25,(0,0,0))
+            texte("team "+str(teamsp[0]+1),760,220,20,(0,0,0))
+        #p4
+        if nbj>=4:
+            bts[24]=bouton(700,260,50,25,(200,200,200))
+            texte("<>",705,260,25,(0,0,0))
+            texte("team "+str(teamsp[0]+1),760,260,20,(0,0,0))
     #modes de jeu
-    #TODO
+    texte("actual game mode : "+gamemodes[mode],350,350,20,(0,0,0))
+    bts[25]=bouton(650,350,50,25,(200,200,200))
+    texte("<>",655,350,25,(0,0,0))
+    if mode==5:
+        bts[26]=bouton(350,390,25,25,(250,0,0))
+        bts[27]=bouton(505,390,25,25,(0,250,0))
+        texte("-",355,390,25,(0,0,0))
+        texte("+",510,390,25,(0,0,0))
+        texte("nb teams : "+str(nbteams),380,390,20,(0,0,0))
+    if mode==2 or mode==5:
+        bts[28]=bouton(350,430,25,25,(250,0,0))
+        bts[29]=bouton(525,430,25,25,(0,250,0))
+        texte("-",355,430,25,(0,0,0))
+        texte("+",530,430,25,(0,0,0))
+        texte("match time : "+str(tps),380,430,20,(0,0,0))
+    if mode==3:
+        bts[30]=bouton(350,390,25,25,(250,0,0))
+        bts[31]=bouton(505,390,25,25,(0,250,0))
+        texte("-",355,390,25,(0,0,0))
+        texte("+",510,390,25,(0,0,0))
+        texte("max size : "+str(paliertaille),380,390,20,(0,0,0))
+    if mode==4:
+        bts[32]=bouton(350,390,25,25,(250,0,0))
+        bts[33]=bouton(555,390,25,25,(0,250,0))
+        texte("-",355,390,25,(0,0,0))
+        texte("+",560,390,25,(0,0,0))
+        texte("max points : "+str(palierpoints),380,390,20,(0,0,0))
     #mode couleurs
     #TODO
     #update screen
@@ -380,24 +529,34 @@ def affmenu(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin):
 
 
 def menu():
+    keysp=[0,1,2,3]
+    teamsp=[0,0,1,1]
     modecl=0 #0=normal , 1=dégradé
     tx,ty=65,50 #petit : 40*30 , moyen : 50*40 , grand : 65*50
-    mode=0
+    mode=0 #0=normal , 1=battle-royale , 2=time match , 3=max size match , 4=max points match , 5=team match
     nbb=10 #min : 0 , max : 15   #bots
     nbj=1 #min : 1 , max : 4    #humains
     dif=0 #facile : 0 , moyen : 1 , difficile : 2 , hardcore : 3
-    tmin=5 #min : 1 , max : 6
+    tmin=3 #min : 1 , max : 6
     affbords=1 #non : 0 , oui : 1
     affquadr=1 #non : 0 , oui : 1
+    tps=300 #min : 60 , max : 600 (en secondes)
+    paliertaille=50 #min : 30 , max : 100
+    palierpoints=30000 #min : 10000 , max : 100000
+    nbteams=2 #min : 2 , max : 4
     encourp=False
     encourm=True
+    needtoaff=True
     while encourm:
-        bts=affmenu(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin)
+        if needtoaff:
+            bts=affmenu(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin,keysp,tps,palierpoints,paliertaille,nbteams,teamsp)
+            needtoaff=False
         for event in pygame.event.get():
             if event.type==QUIT: exit()
             elif event.type==KEYDOWN:
                 if event.key==K_q: exit()
             elif event.type==MOUSEBUTTONUP:
+                needtoaff=True
                 pos=pygame.mouse.get_pos()
                 rpos=pygame.Rect(pos[0],pos[1],1,1)
                 for b in bts:
@@ -420,8 +579,66 @@ def menu():
                             elif tx==50: tx,ty=65,50
                         elif di==11: affbords=not affbords
                         elif di==12: affquadr=not affquadr
+                        elif di==13:
+                            keysp[0]-=1
+                            if keysp[0]<0: keysp[0]=len(pkeys)-1
+                        elif di==14:
+                            keysp[0]+=1
+                            if keysp[0]>len(pkeys)-1: keysp[0]=0
+                        elif di==15:
+                            keysp[1]-=1
+                            if keysp[1]<0: keysp[1]=len(pkeys)-1
+                        elif di==16:
+                            keysp[1]+=1
+                            if keysp[1]>len(pkeys)-1: keysp[1]=0
+                        elif di==17:
+                            keysp[2]-=1
+                            if keysp[2]<0: keysp[2]=len(pkeys)-1
+                        elif di==18:
+                            keysp[2]+=1
+                            if keysp[2]>len(pkeys)-1: keysp[2]=0
+                        elif di==19:
+                            keysp[3]-=1
+                            if keysp[3]<0: keysp[3]=len(pkeys)-1
+                        elif di==20:
+                            keysp[3]+=1
+                            if keysp[3]>len(pkeys)-1: keysp[3]=0
+                        elif di==21:
+                            teamsp[0]+=1
+                            if teamsp[0]>nbteams-1: teamsp[0]=0
+                        elif di==22:
+                            teamsp[1]+=1
+                            if teamsp[1]>nbteams-1: teamsp[1]=0
+                        elif di==23:
+                            teamsp[2]+=1
+                            if teamsp[2]>nbteams-1: teamsp[2]=0
+                        elif di==24:
+                            teamsp[3]+=1
+                            if teamsp[3]>nbteams-1: teamsp[3]=0
+                        elif di==25:
+                            mode+=1
+                            if mode==6: mode=0
+                        elif di==26:
+                            if nbteams > 2 : nbteams-=1
+                        elif di==27:
+                            if nbteams < 4 : nbteams+=1
+                        elif di==28:
+                            if tps > 60 : tps-=60
+                        elif di==29:
+                            if tps < 600 : tps+=60
+                        elif di==30:
+                            if paliertaille > 30 : paliertaille-=5
+                        elif di==31:
+                            if paliertaille < 100 : paliertaille+=5
+                        elif di==32:
+                            if palierpoints > 10000 : palierpoints-=1000
+                        elif di==33:
+                            if palierpoints < 100000 : palierpoints+=1000
+                            
+                            
+                        
     if encourp:
-        men=game(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin)
+        men=game(modecl,tx,ty,mode,nbb,nbj,dif,affbords,affquadr,tmin,keysp,tps,palierpoints,paliertaille,nbteams,teamsp)
         if men: menu()
     
 
